02f9f4a191e75acf8acb3bdd552b8c65af1f3099
Riceball-Laptop <42161586+Anderice@users.noreply.github.com>
2020-05-18 15:56:50 +0800

    v5
    mix

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.namecard').slick({
+ dots: true,
+ infinite: true,
+ speed: 300,
+ slidesToShow: 1,
+ centerMode: true,
+
+ centerMode: true,
+ centerPadding: '60px',
+
+
+ variableWidth: true
+
+
+
+});
